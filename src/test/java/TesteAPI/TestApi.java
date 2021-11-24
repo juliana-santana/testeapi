1fc6c6cd90f9f00826d611580daf99284789af08
Juliana Santana <julianacarlas>
2021-11-23 22:56:06 -0300

    Atualizando teste

--- src/test/java/TesteAPI/TestApi.java
+++ src/test/java/TesteAPI/TestApi.java
@@ -17,13 +17,27 @@ public class TestApi {
     @Test
     @DisplayName("Quando pegar lista de cliente com vários clientes cadastrados, Então informar todos os clientes")
     public void pesqusiandoVariosUsuarios(){
-        String respostaEsperada = "{\"page\":2," +
-                "\"per_page\":6," +
-                "\"total\":12," +
-                "\"total_pages\":2," +
-                "\"data\":[{\"id\":7," +
-                "\"email\":\"michael.lawson@reqres.in\",\"first_name\":\"Michael\",\"last_name\":\"Lawson\",\"avatar\":\"https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg\"},{\"id\":8,\"email\":\"lindsay.ferguson@reqres.in\",\"first_name\":\"Lindsay\",\"last_name\":\"Ferguson\",\"avatar\":\"https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg\"},{\"id\":9,\"email\":\"tobias.funke@reqres.in\",\"first_name\":\"Tobias\",\"last_name\":\"Funke\",\"avatar\":\"https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg\"},{\"id\":10,\"email\":\"byron.fields@reqres.in\",\"first_name\":\"Byron\",\"last_name\":\"Fields\",\"avatar\":\"https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg\"},{\"id\":11,\"email\":\"george.edwards@reqres.in\",\"first_name\":\"George\",\"last_name\":\"Edwards\",\"avatar\":\"https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg\"},{\"id\":12,\"email\":\"rachel.howell@reqres.in\",\"first_name\":\"Rachel\",\"last_name\":\"Howell\",\"avatar\":\"https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg\"}],\"ad\":{\"company\":\"StatusCode Weekly\",\"url\":\"http://statuscode.org/\",\"text\":\"A weekly newsletter focusing on software development, infrastructure, the server, performance, and the stack end of things.\"}}";
-
+        String respostaEsperada = "{\"page\":2,\"per_page\":6," +
+                "\"total\":12,\"total_pages\":2," +
+                "\"data\":[{\"id\":7,\"email\":\"michael.lawson@reqres.in\"," +
+                "\"first_name\":\"Michael\",\"last_name\":\"Lawson\"," +
+                "\"avatar\":\"https://reqres.in/img/faces/7-image.jpg\"}," +
+                "{\"id\":8,\"email\":\"lindsay.ferguson@reqres.in\"," +
+                "\"first_name\":\"Lindsay\",\"last_name\":\"Ferguson\"," +
+                "\"avatar\":\"https://reqres.in/img/faces/8-image.jpg\"}," +
+                "{\"id\":9,\"email\":\"tobias.funke@reqres.in\"," +
+                "\"first_name\":\"Tobias\",\"last_name\":\"Funke\"," +
+                "\"avatar\":\"https://reqres.in/img/faces/9-image.jpg\"}," +
+                "{\"id\":10,\"email\":\"byron.fields@reqres.in\"," +
+                "\"first_name\":\"Byron\",\"last_name\":\"Fields\"," +
+                "\"avatar\":\"https://reqres.in/img/faces/10-image.jpg\"}," +
+                "{\"id\":11,\"email\":\"george.edwards@reqres.in\"," +
+                "\"first_name\":\"George\",\"last_name\":\"Edwards\"," +
+                "\"avatar\":\"https://reqres.in/img/faces/11-image.jpg\"}," +
+                "{\"id\":12,\"email\":\"rachel.howell@reqres.in\",\"first_name\":\"Rachel\"," +
+                "\"last_name\":\"Howell\",\"avatar\":\"https://reqres.in/img/faces/12-image.jpg\"}]," +
+                "\"support\":{\"url\":\"https://reqres.in/#support-heading\"," +
+                "\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
         given().
                 contentType(JSON).
         when().
@@ -39,14 +53,11 @@ public class TestApi {
     @DisplayName("Quando pegar lista de cliente informando apenas um cliente, Então apresentar apenas um cliente")
     public void pesqusandoApenasUmUsuario() {
 
-        String respostaEsperada = "{\"data\":{\"id\":2," +
-                "\"email\":\"janet.weaver@reqres.in\"," +
-                "\"first_name\":\"Janet\"," +
-                "\"last_name\":\"Weaver\"," +
-                "\"avatar\":\"https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg\"}," +
-                "\"ad\":{\"company\":\"StatusCode Weekly\"," +
-                "\"url\":\"http://statuscode.org/\"," +
-                "\"text\":\"A weekly newsletter focusing on software development, infrastructure, the server, performance, and the stack end of things.\"}}";
+        String respostaEsperada = "{\"data\":{\"id\":2,\"email\":\"janet.weaver@reqres.in\"," +
+                "\"first_name\":\"Janet\",\"last_name\":\"Weaver\"," +
+                "\"avatar\":\"https://reqres.in/img/faces/2-image.jpg\"}," +
+                "\"support\":{\"url\":\"https://reqres.in/#support-heading\"," +
+                "\"text\":\"To keep ReqRes free, contributions towards server costs are appreciated!\"}}";
 
         given().
                 contentType(JSON).
